ab08e3e67ae4e4cca78462ce3abd37ffd3b1ad47
Aysan Isayo <aisayo@icloud.com>
2019-12-12 17:37:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,5 +11,10 @@ def countdown(integer)
 end
 
 def countdown_with_sleep(integer)
-  
+  while integer > 0 
+   puts "#{integer} SECOND(S)!"
+   sleep(5)
+   integer -= 1
+  end 
+  "HAPPY NEW YEAR!"
 end 
